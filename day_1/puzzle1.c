6d1beb7c0a0bd9f81c1a23fefb3771e63cb28aca
lmbt <l3mb0t@gmail.com>
2021-12-11 10:30:05 -0500

    Day 1 - 1 solution

--- day_1/puzzle1.c
+++ day_1/puzzle1.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Input numbers should only go up to 5 digits (+1 for \n, +1 for \0)
+#define CHARS_PER_LINE 7
 
 int main (){
 
   FILE* fileP;
-  int parsedChars = 0;
-  char lastLine [3];
-  char currLine [3];
+  int parsedLines = 0;
+  int incReadings = 0;
+  char cbCurr[CHARS_PER_LINE];
+  char cbNext[CHARS_PER_LINE];
 
   // Exit if can't find input file
   fileP = fopen("puzzle1_input.txt", "r");
@@ -14,7 +20,24 @@ int main (){
     return 1;
   }
 
-  // Grab next line, compare to last (fseek)
+  // Read first line
+  if (fgets(cbCurr, CHARS_PER_LINE, fileP) == NULL) {
+    printf("Unexpected end of input.\n");
+    return 1;
+  }
+
+  // Do until EOF
+  while(fgets(cbNext, CHARS_PER_LINE, fileP) != NULL) {
+
+    // Compare next line to current line
+    if (atoi(cbNext) > atoi(cbCurr)) {
+      incReadings++;
+    }
+
+    strcpy(cbCurr, cbNext);
+  }
+
+  printf("Readings: %i\n", incReadings);
 
   return 0;
 }
